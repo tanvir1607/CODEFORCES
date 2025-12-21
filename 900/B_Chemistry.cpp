aa121d5eda71632e40f381b682d5c316ae23d57e
tanvir1607 <10viranik@gmail.com>
2025-12-21 23:28:36 +0600

    CP

---
+++ 900/B_Chemistry.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+#define endl '\n'
+#define optimize() ios::sync_with_stdio(false); cin.tie(nullptr);
+
+int main() 
+{
+    optimize();
+
+    int test;
+    cin >> test;
+    while (test--)
+    {
+        int size, del;
+        string str;
+        cin >> size >> del >> str;
+
+        int oddPairs = 0;
+        set<char> Set(str.begin(), str.end());
+        for (char ch : Set)
+            oddPairs += count(str.begin(), str.end(), ch) % 2;
+        
+        cout << (oddPairs - del <= 1 ? "YES" : "NO") << endl;        
+    }
+
+    return 0;
+}
+
+
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl '\n'
+#define optimize() ios::sync_with_stdio(false); cin.tie(nullptr);
+
+int main()
+{
+    optimize();
+
+    int test;
+    cin >> test;
+    while (test--)
+    {
+        int size, del;
+        string str;
+        cin >> size >> del >> str;
+
+        map<char, int> freq;
+        for (char ch : str) freq[ch]++;
+
+        int oddPairs = 0;
+        for (auto [key, val] : freq) oddPairs += val % 2;
+        cout << (oddPairs - del <= 1 ? "YES" : "NO") << endl;
+    }
+
+    return 0;
+}
