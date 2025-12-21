0eaade3cc6f52d9832132e9016938cd0cfd5d74b
tanvir1607 <10viranik@gmail.com>
2025-12-22 00:45:45 +0600

    CP

---
+++ 800/C_Cypher.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+#define endl '\n'
+#define optimize() ios::sync_with_stdio(false); cin.tie(nullptr);
+
+int main() 
+{
+    optimize();
+
+    int test;
+    cin >> test;
+    while (test--)
+    {
+        int size;
+        cin >> size;
+        vector<int> vec(size);
+        for (int &x : vec) cin >> x;
+
+        for (int x : vec)
+        {
+            int val;
+            cin >> val;
+            string change;
+            cin >> change;
+            for (char ch : change) x = (x + (ch == 'D' ? 1 : -1) + 10) % 10;
+            cout << x << " ";
+        }     
+        cout << endl;  
+    }
+
+    return 0;
+}
+
+
